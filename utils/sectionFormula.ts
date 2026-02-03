9f2a73dfd1146b83c2d4876ad4043b58a2433f9c
javierogamm <javier.garcia.araez@gmail.com>
2026-02-03 07:48:22 +0100

    Revert "Accept spaced condition formulas in section notes"

--- utils/sectionFormula.ts
+++
@@ -1,4 +0,0 @@
-const SECTION_CONDITION_REGEX = /condition\s*:\s*\(/gi;
-
-export const normalizeSectionConditionSpacing = (value: string) =>
-  value.replace(SECTION_CONDITION_REGEX, "condition:(");
